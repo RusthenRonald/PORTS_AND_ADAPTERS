fb86aafd122cdb1b4e85d010b38096bf1ccb867a
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-10-31 01:04:52 -0300

    feat: implement basic Vue directives (v-for,v-bind,-v-model,v-on)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-const todos : any= [
+import { ref,reactive } from 'vue';
+const todos : any= reactive([
   {description: 'Estudar Typescript', done: true},
   {description: 'Fazer a prova Online', done: false},
   {description: 'Cortar a grama', done: false},
-]
+])
+const description = ref('');
+
+function addItem (){
+   todos.push({description: description.value,done:false})
+   description.value='';
+}
 </script>
 
 <template>
-  {{ todos }}
+  <div v-for="item in todos">
+    <p><span :style="{'text-decoration':(item.done)?'line-through':''}">{{item.description}}</span> {{ item.done }}</p>
+  </div>
+  {{ description }}
+  <input type="text" v-model="description"/>
+  <button v-on:click="addItem()">Add</button>
 </template>
 
 <style scoped>
